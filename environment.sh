6f00eecf6bd70715da09146fd2f0a6ff81aad023
Joaquin <Joaquin.Quintana@colorado.edu>
2021-11-05 12:44:08 -0600

    new requirements

--- environment.sh
+++ environment.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+sudo rm -r venv
+python3 -m venv venv
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 source venv/bin/activate
 export FLASK_APP=Monkey.py
 export FLASK_ENV=development
