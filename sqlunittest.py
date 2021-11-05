18b9c3519704e77e2af63662beeaad3b8beb294d
alxthmsCO <90875291+alxthmsCO@users.noreply.github.com>
2021-11-05 14:26:29 -0600

    Update sqlunittest.py

--- sqlunittest.py
+++ sqlunittest.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-#import init_db
+import init_db
 import os
 import sqlite3
 
@@ -28,14 +28,30 @@ class storedbTestCase(unittest.TestCase):
         pass
             
     def test_init(self):
-        pass
+        init_db
+        if not os.path.exists("database.db"):
+            raise AssertionError(".db does not exist")
+        else:
+            pass
 
     def test_name(self):
         conn = sqlite3.connect("database.db")
         cursor = conn.execute("SELECT uname from contacts WHERE id = 1")
         self.assertEqual(cursor.fetchone()[0], 'First Name')
         conn.close()
+
+    def test_email(self):
+        conn = sqlite3.connect("database.db")
+        cursor = conn.execute("SELECT email from contacts WHERE id = 2")
+        self.assertEqual(cursor.fetchone()[0], 'second@gmail')
+        conn.close()
         
+    def test_content(self):
+        conn = sqlite3.connect("database.db")
+        cursor = conn.execute("SELECT content from contacts WHERE email = 'first@gmail'")
+        self.assertEqual(cursor.fetchone()[0], 'Content for the first name')
+        conn.close()
+
 # Main: Run Test Cases
 if __name__ == '__main__':
     unittest.main()
